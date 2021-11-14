ee05ed2a8f6d49b9b7bd41488648d1588c47e18a
abdallah alabed <abd.alabed97@gmail.com>
2021-11-14 22:56:46 +0200

    dequeue not working

--- python/stack-and-queue/stack_and_queue/queue.py
+++ python/stack-and-queue/stack_and_queue/queue.py
@@ -1,3 +1,6 @@
+from stack_and_queue.stack import Stack
+
+
 class Node():
     def __init__(self, value="",next=None):
         self.value = value
@@ -39,3 +42,46 @@ class Queue():
             return True
         else:
             return False
+
+class PseudoQueue():
+    def __init__(self):
+        self.stack1 = Stack() 
+        self.stack2 = Stack()
+
+    def enqueue(self, value):
+        c_top=self.stack2.top
+        self.stack1.push(value)
+        b_top = self.stack1.top
+        self.stack1.pop()   
+        self.stack2.push(b_top)
+        if c_top != None:
+            self.stack2.top.next=c_top
+
+    def dequeue(self):
+        while self.stack2.top != None:
+            print(self.stack2.top)
+            c_top=self.stack2.top
+            self.stack2.pop()
+            self.stack1.push(c_top)
+            self.stack2.top=c_top.next
+        print(self.stack1.top)
+        c_top=self.stack1.top
+        self.stack1.pop()
+        print(self.stack1.top)
+        while self.stack1.top != None:
+            if c_top.next != None:
+                self.stack1.top=c_top.next
+            self.stack1.pop()
+            self.stack2.push(c_top)
+            self.stack1.top=self.stack1.top.next
+        print(self.stack2.top)
+
+
+    def __str__(self):
+        check=self.stack2.top
+        outputStr=""
+        while check:
+            outputStr +=f"{vars(check.value)['value']} -> "
+            check = check.next
+        outputStr += 'NULL'
+        return outputStr
